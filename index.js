b1c3df6d6b7be21aa6b6eb6288d4eec59b908732
andrew <andrew@textile.io>
2020-03-18 11:39:58 -0700

    add list pins
    Signed-off-by: andrew <andrew@textile.io>

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const core = require('@actions/core');
 const addPin = require('./addPin');
 const getOptions = require('./getOptions');
 
-const run = async () => {
+const run = () => {
   const cid = core.getInput('cid');
   const pinata_key = core.getInput('pinata_key');
   const pinata_secret = core.getInput('pinata_secret');
   const options = getOptions();
-  await addPin(cid, options, pinata_key, pinata_secret);
+  return addPin(cid, options, pinata_key, pinata_secret).then((result) => {
+    return result
+  });
 }
 
-await run();
+run();
