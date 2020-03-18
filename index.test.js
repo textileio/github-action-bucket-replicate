cd0585ddafb0d16a22eb26ea4cec018557c7298e
andrew <andrew@textile.io>
2020-03-18 10:45:37 -0700

    add pin test
    Signed-off-by: andrew <andrew@textile.io>

--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const addPin = require('./addPin');
 const getOptions = require('./getOptions');
 
 test('get options', async() => {
@@ -6,4 +7,12 @@ test('get options', async() => {
   const options = getOptions();
   expect(options.pinataMetadata).toBeTruthy();
   expect(options.pinataMetadata.keyvalues.bucket).toEqual(bucket);
+});
+
+test('replicate bucket', async() => {
+  const cid = core.getInput('cid');
+  const options = getOptions();
+  expect(function() {
+    addPin(cid, options)
+  }).not.toThrow();
 });
