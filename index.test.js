9cd232f56200c536f1cf051329a7bdc58980e02c
andrew <andrew@textile.io>
2020-03-18 10:41:47 -0700

    try with process
    Signed-off-by: andrew <andrew@textile.io>

--- index.test.js
+++ index.test.js
@@ -1,11 +1,9 @@
-const process = require('process');
 const core = require('@actions/core');
 const getOptions = require('./getOptions');
 
 test('get options', async() => {
-  const pinata_name = core.getInput('pinata_name');
-  process.env['pinata_name'] = pinata_name;
+  const bucket = core.getInput('bucket');
   const options = getOptions();
   expect(options.pinataMetadata).toBeTruthy();
-  expect(options.pinataMetadata.name).toEqual('ci-test-pin');
+  expect(options.pinataMetadata.keyvalues.bucket).toEqual(bucket);
 });
