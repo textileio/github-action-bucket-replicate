fabf2f892a1f22b4aec319b78af16a6b14a097ff
andrew <andrew@textile.io>
2020-03-18 11:32:38 -0700

    add list pins
    Signed-off-by: andrew <andrew@textile.io>

--- addPin.js
+++ addPin.js
@@ -3,14 +3,9 @@ const pinataSDK = require('@pinata/sdk');
 
 const addPin = async (cid, options, pinata_key, pinata_secret) => {
   const pinata = pinataSDK(pinata_key, pinata_secret);
-  return pinata.testAuthentication().then(() => {
-    return pinata.addHashToPinQueue(cid, options).then((result) => {
-      //handle results here
-      console.log(result);
-      core.setOutput("pinata_id", result.id);
-      return result.id;
-    })
-  })
+  const result = await pinata.addHashToPinQueue(cid, options)
+  core.setOutput("pinata_id", result.id);
+  return result.id;
 };
 
 module.exports = addPin;
