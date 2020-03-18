a6b64601917991548e8c442fb3ae6149f6c87348
andrew <andrew@textile.io>
2020-03-18 11:09:55 -0700

    add pin test
    Signed-off-by: andrew <andrew@textile.io>

--- index.test.js
+++ index.test.js
@@ -1,15 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const addPin = require('./addPin');
-const getOptions = require('./getOptions');
 
 test('replicate bucket', () => {
 
-  const bucket = process.env.bucket;
+  const bucket = `${process.env.bucket}`;
   expect(bucket).toEqual('jekyll-ipfs-blog');
   const cid = process.env.cid;
   const pinata_name = process.env.pinata_name;
-  const commit = github.context.payload.head;
+  const commit = `${github.context.payload.head}`;
 
   const pinata_key = process.env.pinata_key;
   const pinata_secret = process.env.pinata_secret;
