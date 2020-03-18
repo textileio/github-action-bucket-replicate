c61d106bd1c2c0978bea62b430c5aef0297c5446
andrew <andrew@textile.io>
2020-03-18 10:55:11 -0700

    add pin test
    Signed-off-by: andrew <andrew@textile.io>

--- addPin.js
+++ addPin.js
@@ -5,11 +5,13 @@ const addPin = async (cid, options) => {
   const pinata_key = core.getInput('pinata_key');
   const pinata_secret = core.getInput('pinata_secret');
   const pinata = pinataSDK(pinata_key, pinata_secret);
-  return pinata.addHashToPinQueue(cid, options).then((result) => {
+  return pinata.testAuthentication().then(() => {
+    return pinata.addHashToPinQueue(cid, options).then((result) => {
       //handle results here
       console.log(result);
       core.setOutput("pinata_id", result.id);
       return result;
+    })
   })
 };
 
