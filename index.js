931ad6f778ca885ca9a2961352033f0f72c3cf58
andrew <andrew@textile.io>
2020-03-18 12:31:32 -0700

    fix payload strings
    Signed-off-by: andrew <andrew@textile.io>

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const addPin = require('./addPin');
-const getOptions = require('./getOptions');
 
 const run = () => {
   const cid = core.getInput('cid');
   const pinata_key = core.getInput('pinata_key');
   const pinata_secret = core.getInput('pinata_secret');
-  const options = getOptions();
+
+
+  const bucket = `${core.getInput('bucket')}`;
+  const pinata_name = core.getInput('pinata_name');
+
+  // const unpin = core.getInput('unpin');
+
+  const commit = `${github.context.payload.head}`;
+
+  // todo: add textile host_nodes multiaddress
+  const options = {
+    pinataMetadata: {
+        name: pinata_name,
+        keyvalues: {
+            bucket: bucket,
+            commit: commit
+        }
+    }
+  };
+
   addPin(cid, options, pinata_key, pinata_secret).then((result) => {
     return result
   });
