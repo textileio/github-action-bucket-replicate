3b312e2131af67b3638293e60c8e49e9fc2062a5
andrew <andrew@textile.io>
2020-03-18 11:29:18 -0700

    add list pins
    Signed-off-by: andrew <andrew@textile.io>

--- index.test.js
+++ index.test.js
@@ -8,14 +8,13 @@ test('replicate bucket', async () => {
   const bucket = `${process.env.bucket}`;
   expect(bucket).toEqual('jekyll-ipfs-blog');
   const cid = process.env.cid;
-  const pinata_name = process.env.pinata_name || 'textile_bucket_ci';
+  // const pinata_name = process.env.pinata_name || 'textile_bucket_ci';
   const commit = `${github.context.payload.head}`;
 
   const pinata_key = process.env.pinata_key;
   const pinata_secret = process.env.pinata_secret;
   const options = {
     pinataMetadata: {
-        name: pinata_name,
         keyvalues: {
             bucket: bucket,
             commit: commit
